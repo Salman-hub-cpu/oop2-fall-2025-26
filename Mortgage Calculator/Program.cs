2545d24adc9563e1999973479f6fd1d093698d90
Salman Farsy <149928009+Salman-hub-cpu@users.noreply.github.com>
2025-11-05 14:31:09 +0600

    22-48037-2

---
+++ Mortgage Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        double principal, annualRate, monthlyRate, monthlyPayment, totalPaid, totalInterest;
+        int years, totalPayments;
+
+        Console.Write("Enter loan amount: ");
+        principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter annual interest rate (%): ");
+        annualRate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter loan term (years): ");
+        years = Convert.ToInt32(Console.ReadLine());
+
+        monthlyRate = (annualRate / 100) / 12;
+        totalPayments = years * 12;
+
+        monthlyPayment = principal * (monthlyRate * Math.Pow(1 + monthlyRate, totalPayments))
+                        / (Math.Pow(1 + monthlyRate, totalPayments) - 1);
+
+        totalPaid = monthlyPayment * totalPayments;
+        totalInterest = totalPaid - principal;
+
+        CultureInfo bdCulture = new CultureInfo("bn-BD");
+
+        Console.WriteLine($"\nMonthly Payment: {monthlyPayment.ToString("C", bdCulture)}");
+        Console.WriteLine($"Total Paid: {totalPaid.ToString("C", bdCulture)}");
+        Console.WriteLine($"Total Interest: {totalInterest.ToString("C", bdCulture)}");
+    }
+}
