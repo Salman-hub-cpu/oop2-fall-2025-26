e8fbb3698688f71b399fda9880f2f5860742913d
Salman Farsy <149928009+Salman-hub-cpu@users.noreply.github.com>
2025-12-15 23:53:14 +0600

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IPrintable
+{
+    void Print();
+}
+
+interface ISerializable
+{
+    void Save();
+}
+
+class Report : IPrintable, ISerializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Report Printed");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Report Saved");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Report r = new Report();
+        r.Print();
+        r.Save();
+    }
+}
