f3300adc365769646d4436c3e71e61f86bafe54d
Salman Farsy <149928009+Salman-hub-cpu@users.noreply.github.com>
2025-12-22 14:38:38 +0600

    22-48037-2

---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DelegateExample
+{
+   
+    delegate int Operation(int x, int y);
+
+    class Program
+    {
+      
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Calculate(int a, int b, Operation op)
+        {
+            return op(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = Calculate(10, 5, Add);
+            Console.WriteLine("Addition Result: " + sum);
+
+            int difference = Calculate(10, 5, Subtract);
+            Console.WriteLine("Subtraction Result: " + difference);
+
+            Console.ReadLine();
+        }
+    }
+}
